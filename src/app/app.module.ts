bb769c17fade8170f747f74aa5c32ac419dffa1f
andenson-silva <andenson.j.silva@gmail.com>
2022-06-07 00:40:52 -0300

    :sparkles: add rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { CoreModule } from './core';
+import { FooterModule } from './shared/footer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
